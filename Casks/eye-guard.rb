90a6f808c3ee328af8e7072d3a5a1d4b0fa0b8e1
MashellHan <MashellHan@users.noreply.github.com>
2026-04-16 20:50:49 +0800

    chore: bump eye-guard to v3.1.1

--- Casks/eye-guard.rb
+++ Casks/eye-guard.rb
@@ -1,6 +1,6 @@
 cask "eye-guard" do
-  version "3.1"
-  sha256 "38e3e5579813d38515ea505879b9278812e1b9b1d4c45ef15cdf8fa7dca31231"
+  version "3.1.1"
+  sha256 "3a57262ed579a2e1c2262df4113182ca6a8ef8238c1690fbbbc0f47da565622f"
 
   url "https://github.com/MashellHan/eye-guard/releases/download/v#{version}/EyeGuard.dmg"
   name "EyeGuard"
